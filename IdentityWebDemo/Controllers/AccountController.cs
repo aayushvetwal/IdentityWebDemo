423c00e7288205db1846d82f535e91fc55fc4217
Aayush Vetwal <aayushvetwal@outlook.com>
2018-11-12 18:57:11 +0545

    Add project files.

---
+++ IdentityWebDemo/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
+
+namespace IdentityWebDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        public UserManager<IdentityUser> UserManager => HttpContext.GetOwinContext().Get<UserManager<IdentityUser>>();
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            //Do this to avoid "username is already taken" validation
+            //begin
+            var identityUser = await UserManager.FindByNameAsync(model.Username);
+            if(identityUser != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            //end
+
+            var identityResult = await UserManager.CreateAsync(new IdentityUser(model.Username), model.Password);
+
+            if (identityResult.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", identityResult.Errors.FirstOrDefault());
+
+            return View(model);
+        }
+    }
+
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ IdentityWebDemo/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using Microsoft.AspNet.Identity;
+
+[assembly: OwinStartup(typeof(IdentityWebDemo.Startup))]
+
+namespace IdentityWebDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            const string connectionString =
+                @"Data Source=AAYUSHVETWAL\SQLEXPRESS;Database=IdentityDemo.Module2.2;trusted_connection=yes";
+            app.CreatePerOwinContext(() => new IdentityDbContext(connectionString));
+            app.CreatePerOwinContext<UserStore<IdentityUser>>((opt, cont) => new UserStore<IdentityUser>(cont.Get<IdentityDbContext>()));
+            app.CreatePerOwinContext<UserManager<IdentityUser>>((opt, cont) => new UserManager<IdentityUser>(cont.Get<UserStore<IdentityUser>>()));
+        }
+    }
+}
